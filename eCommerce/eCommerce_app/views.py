cd2638685efb8e0c7c25a04bc21ea5b2ad29b112
Dhairya Soni <dvsoni2711@gmail.com>
2021-09-16 15:55:56 -0400

    updated NavBar according to userroll

--- eCommerce/eCommerce_app/views.py
+++ eCommerce/eCommerce_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.db import IntegrityError
 from .models import User, Category, Comment, Cart
@@ -10,7 +10,11 @@ from .models import BuyerOrSeller as BOS
 
 # Create your views here.
 def index(req):
-    return render(req, "index/index.html")
+    if req.user.is_authenticated:
+        arr = check_roll(req.user)
+        return render(req, "index/index.html", arr)
+    else:
+        return render(req, 'index/index.html')
 
 
 def login_view(request):
@@ -46,21 +50,48 @@ def register(request):
         # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+        buyer = request.POST.get("buyer")
+        seller = request.POST.get("seller")
+        if buyer != None or seller != None:
+            if password != confirmation:
+                return render(request, "index/register.html", {
+                    "message": "Passwords must match."
+                })
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
+            # Attempt to create new user
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                b = False
+                s = False
+                if buyer is not None and seller is not None:
+                    b = True
+                    s = True
+                elif buyer is not None and seller is None:
+                    b = True
+                    s = False
+                elif buyer is None and seller is not None:
+                    b = False
+                    s = True
+                bos = BOS(user=user, buyer=b, seller=s)
+                bos.save()
+            except IntegrityError:
+                return render(request, "index/register.html", {
+                    "message": "Username already taken."
+                })
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "index/register.html", {
+                "message": "Select your roll as a seller or as a buyer."
             })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "index/register.html")
 
+
+def check_roll(user):
+    bos = BOS.objects.get(user=user)
+    buyer = bos.buyer
+    seller = bos.seller
+    print(buyer, " --- ", seller)
+    return {"buyer": buyer, "seller": seller}
